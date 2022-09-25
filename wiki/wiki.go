22987049828d8901a096652b247d4ec48b428165
zhangzongliang <zhangzongliang@zuoshouyisheng.com>
2022-09-25 17:12:50 +0800

    Feat: wiki init web service

--- wiki/wiki.go
+++ wiki/wiki.go
@@ -1,33 +1,93 @@
+/**
+ * @Author: Noaghzil
+ * @Date:   2022-09-25 15:32:17
+ * @Last Modified by:   Noaghzil
+ * @Last Modified time: 2022-09-25 17:09:44
+ */
 // 读写文件
 package main
 
 import (
-    "fmt"
-    "os"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
 )
 
 type Page struct {
-    Title string
-    Body  []byte
+	Title string
+	Body  []byte
 }
 
 func (p *Page) save() error {
-    filename := p.Title + ".txt"
-    return os.WriteFile(filename, p.Body, 0600)
+	filename := p.Title + ".txt"
+	return os.WriteFile(filename, p.Body, 0600)
 }
 
 func loadPage(title string) (*Page, error) {
-    filename := title + ".txt"
-    body, err := os.ReadFile(filename)
-    if err != nil {
-        return nil, err
-    }
-    return &Page{Title: title, Body: body}, nil
+	filename := title + ".txt"
+	body, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
 }
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi, %s!", r.URL.Path[1:])
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	t, _ := template.ParseFiles(tmpl + ".html")
+	t.Execute(w, p)
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	}
+	renderTemplate(w, "view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	renderTemplate(w, "edit", p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/save/"):]
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	p.save()
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
+// func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+// 	return func(w http.ResponseWriter, r *http.Request) {
+// 		m := validPath.FindStringSubmatch(r.URL.Path)
+// 		if m == nil {
+// 			http.NotFound(w, r)
+// 			return
+// 		}
+// 		fn(w, r, m[2])
+// 	}
+// }
+
 func main() {
-    p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
-    p1.save()
-    p2, _ := loadPage("TestPage")
-    fmt.Println(string(p2.Body))
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", saveHandler)
+	log.Fatal(http.ListenAndServe(":6150", nil))
+	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
+	p1.save()
+
 }
